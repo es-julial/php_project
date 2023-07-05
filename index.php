eeeecea89478e1e124287ddab5874627ef6700a9
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-07-05 17:34:11 +0200

    3. UI Design___3. Header

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
 
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -32,6 +32,23 @@
     </div>
   </div>
 </nav>
+
+
+<header id="header">
+    <div class="intro">
+        <div class="overlay">
+            <div class="container">
+                <div class="row">
+                    <div class="intro-text">
+                        <h1>W&S</h1>
+                        <p>Restaurant / Coffee / Breakfast</p>
+                        <a href="#about" class="btn btn-custom btn-lg page-scroll">Discover Our Story</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
